ad62c8221d9da11f5b4e8912439199118c9d8057
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2019-01-29 22:55:26 -0500

    remove bootstrap, restyle, and remove photos

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,32 +7,21 @@ module.exports = function(grunt) {
 				options: {
 					engine: 'im',
 					sizes: [
-          {
-            name: 'extra-small',
-            width: 50,
-            suffix: '_1x',
-            quality: 20
-          },{
-						name: 'small',
-						width: 200,
-						suffix: '_1x',
-						quality: 20
-					},
-					{
-						name: 'medium',
-						width: 300,
-						suffix: '_3x',
-						quality: 40
-					},
-          {
-            name: 'large',
-            width: 1024,
-            height: 1024,
-            suffix: '_4x',
-            quality: 60
-          }
-          ]
+				{
+					name: 'medium',
+					width: 300,
+					suffix: '_3x',
+					quality: 40
 				},
+	          {
+	            name: 'large',
+	            width: 1024,
+	            height: 1024,
+	            suffix: '_4x',
+	            quality: 60
+	          }
+          ]
+		},
 				files: [{
 					expand: true,
 					src: ['*.{gif,jpg,png}'],
--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 // HAMBURGER ICON AND NAVBAR
 const hamburger = document.querySelector('.hamburger');
 const hamburgerNav = document.querySelector('.hamburger-nav');
-const nav = document.querySelector('.navbar-wagon');
+const nav = document.querySelector('.navbar');
 
-const responsiveNav = () => {
- if (matchMedia) {
-  const mq = window.matchMedia("(min-width: 700px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-  }
-};
-function WidthChange(mq) {
-  if (mq.matches) {
-    hamburger.style.visibility = 'visible';
-    hamburgerNav.style.visibility = 'visible';
-    nav.style.visibility = 'visible';
-  } else {
-    // window width is less than 700px
-    hamburger.style.visibility = 'hidden';
-    hamburgerNav.style.visibility = 'hidden';
-    nav.style.visibility = 'hidden';
-  }
+// const responsiveNav = () => {
+//  if (matchMedia) {
+//   const mq = window.matchMedia("(min-width: 700px)");
+//   mq.addListener(WidthChange);
+//   WidthChange(mq);
+//   }
+// };
+// function WidthChange(mq) {
+//   if (mq.matches) {
+//     hamburger.style.visibility = 'visible';
+//     hamburgerNav.style.visibility = 'visible';
+//     nav.style.visibility = 'visible';
+//   } else {
+//     // window width is less than 700px
+//     hamburger.style.visibility = 'hidden';
+//     hamburgerNav.style.visibility = 'hidden';
+//     nav.style.visibility = 'hidden';
+//   }
 
-}
+// }
 
-responsiveNav();
+// responsiveNav();
 
 hamburger.addEventListener('click', () => {
   hamburgerNav.classList.toggle('show-hamburger');
@@ -49,26 +49,26 @@ const projects = [
   //   "url": "https://www.wagefinder.today/",
   //   "description": "A mobile-only app that helps IT workers find out what they're worth."
   // },
-  {
-    "name": "Arcade Game",
-    "image": "images/arcade_game-medium_3x.png",
-    "url": "https://anthonyzamarro.github.io/Arcade-Game/",
-    "description": "Imagine the classic 'Frogger' game, but with \
-      bugs instead of trucks."
-  },
+  // {
+  //   "name": "Arcade Game",
+  //   "image": "images/arcade_game-medium_3x.png",
+  //   "url": "https://anthonyzamarro.github.io/Arcade-Game/",
+  //   "description": "Imagine the classic 'Frogger' game, but with \
+  //     bugs instead of trucks."
+  // },
   {
     "name": "Local Weather",
     "image": "images/local_weather-medium_3x.png",
     "url": "https://anthonyzamarro.github.io/az-local-weather/",
     "description": "See your local weather with this simple app!"
   },
-  {
-    "name": "Eat: Boston",
-    "image": "images/boston_foursquare1-medium_3x.png",
-    "url": "https://anthonyzamarro.github.io/neighborhood_map/",
-    "description": "A Google map that uses the Foursquare API  \
-      to show you what great restaurants are in Boston."
-  },
+  // {
+  //   "name": "Eat: Boston",
+  //   "image": "images/boston_foursquare1-medium_3x.png",
+  //   "url": "https://anthonyzamarro.github.io/neighborhood_map/",
+  //   "description": "A Google map that uses the Foursquare API  \
+  //     to show you what great restaurants are in Boston."
+  // },
   {
     "name": "Twitch TV",
     "image": "images/twitch_tv-medium_3x.png",
@@ -100,7 +100,7 @@ const buildProjectList = () => {
           <div class="project-description">
           </div>
           <img class="project-image" src="${project.image}">
-          <a class="project-link" href="${project.url}" ></a>
+          <a class="project-link" href="${project.url}" target="_blank"></a>
         </li>`
       );
   });
